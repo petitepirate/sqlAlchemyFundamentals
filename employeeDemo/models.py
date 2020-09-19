7090f9d5469f29a8cae3e49bc99c774292277981
megan <msmeganmcmanus@gmail.com>
2020-09-18 17:43:16 -0700

    update demo model

--- employeeDemo/models.py
+++ employeeDemo/models.py
@@ -66,38 +66,6 @@ class Department(db.Model):
         return f"<Department {self.dept_code} {self.dept_name}>"
 
 
-class Project(db.Model):
-    """Project. Employees can be assigned to this."""
-
-    __tablename__ = "projects"
-
-    proj_code = db.Column(db.Text, primary_key=True)
-    proj_name = db.Column(db.Text,
-                          nullable=False,
-                          unique=True)
-
-    # direct navigation: proj -> employeeproject & back
-    assignments = db.relationship('EmployeeProject',
-                                  backref='project')
-
-    def __repr__(self):
-        return f"<Project {self.proj_code} {self.proj_name}>"
-
-
-class EmployeeProject(db.Model):
-    """Mapping of an employee to a project."""
-
-    __tablename__ = "employees_projects"
-
-    emp_id = db.Column(db.Integer,
-                       db.ForeignKey("employees.id"),
-                       primary_key=True)
-    proj_code = db.Column(db.Text,
-                          db.ForeignKey("projects.proj_code"),
-                          primary_key=True)
-    role = db.Column(db.Text)
-
-
 ##############################################################################
 # Example queries
 
@@ -153,59 +121,3 @@ def phone_dir_join_outerjoin():
             print(emp.name, dept.dept_name, dept.phone)
         else:
             print(emp.name, "-", "-")
-
-
-##############################################################################
-# Helper functions
-
-# def example_data():
-#     """Create some sample data."""
-
-#     # In case this is run more than once, empty out existing data
-#     EmployeeProject.query.delete()
-#     Employee.query.delete()
-#     Department.query.delete()
-#     Project.query.delete()
-
-#     # Add sample employees and departments
-#     df = Department(dept_code='fin', dept_name='Finance', phone='555-1000')
-#     dl = Department(dept_code='legal', dept_name='Legal', phone='555-2222')
-#     dm = Department(dept_code='mktg', dept_name='Marketing', phone='555-9999')
-
-#     leonard = Employee(name='Leonard', dept=dl)
-#     liz = Employee(name='Liz', dept=dl)
-#     maggie = Employee(name='Maggie', state='DC', dept=dm)
-#     nadine = Employee(name='Nadine')
-
-#     db.session.add_all([df, dl, dm, leonard, liz, maggie, nadine])
-#     db.session.commit()
-
-#     pc = Project(proj_code='car', proj_name='Design Car',
-#                  assignments=[EmployeeProject(emp_id=liz.id, role='Chair'),
-#                               EmployeeProject(emp_id=maggie.id)])
-#     ps = Project(proj_code='server', proj_name='Deploy Server',
-#                  assignments=[EmployeeProject(emp_id=liz.id),
-#                               EmployeeProject(emp_id=leonard.id, role='Auditor')])
-
-#     db.session.add_all([ps, pc])
-#     db.session.commit()
-
-
-# def connect_db(app):
-#     """Connect the database to our Flask app."""
-
-#     db.app = app
-#     db.init_app(app)
-
-
-# if __name__ == "__main__":
-#     # As a convenience, if we run this module interactively, it will leave
-#     # you in a state of being able to work with the database directly.
-
-#     # So that we can use Flask-SQLAlchemy, we'll make a Flask app
-#     from app import app
-#     connect_db(app)
-
-#     db.drop_all()
-#     db.create_all()
-#     example_data()
